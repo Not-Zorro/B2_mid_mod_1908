425c4091bf6eada5c38bee04eb24d0c1e8b25a8b
David Lopez <ldavid.x.lopezl@gmail.com>
2019-10-20 13:11:34 -0600

    Add student index page and spec

---
+++ spec/features/students/index_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe "Student index page" do
+  it "can list student name, age, cohord, and all instructors" do
+    meg = Instructor.create(name: 'Meg')
+    mike = Instructor.create(name: 'Mike')
+    ian = Instructor.create(name: 'Ian')
+    david = Student.create(
+      name: 'David',
+      age: 19,
+      cohort: 1908
+    )
+    meg.students << david
+    mike.students << david
+
+    visit '/students'
+
+    within "#student-#{david.id}" do
+      expect(page).to have_content('David')
+      expect(page).to have_content('19')
+      expect(page).to have_content('1908')
+      expect(page).to have_content('Instructors:')
+      expect(page).to have_link('Meg')
+      expect(page).to have_link('Mike')
+      expect(page).to_not have_link('Ian')
+    end
+  end
+end
